9454bd9ccb247c156a431fd5a3ebae1c69c97d32
18Venus <149051659+18Venus@users.noreply.github.com>
2024-05-30 20:53:28 +0530

    Edit feature!

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import{BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './navbar.js';
 import Home from './home.js';
 import Create from './create.js';
 import BlogDetails from './blogdetails.js';
 import NotFound from './notfound.js';
+import Edit from './edit.js';
 
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <div className="content">
-       <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route path="/create">
-          <Create/>
-        </Route>
-        <Route path="/blogs/:id">
-          <BlogDetails/>
-        </Route>
-        <Route path="*">
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/edit/:id">
+              <Edit />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
               <NotFound />
             </Route>
-       </Switch>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
--- src/edit.js
+++ src/edit.js
@@ -1,31 +1,48 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
 
-
-const Create = () => {
+const Edit = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
-  
   const history = useHistory();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/blogs/${id}`)
+      .then(res => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then(data => {
+        setTitle(data.title);
+        setBody(data.body);
+        setAuthor(data.author);
+      })
+      .catch(err => {
+        console.error(err);
+        history.push('/'); // Redirect to home if there's an error
+      });
+  }, [id, history]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    fetch('http://localhost:8000/blogs/', {
-      method: 'POST',
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: 'PATCH',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
     }).then(() => {
-      // history.go(-1);
       history.push('/');
-    })
+    });
   }
 
   return (
     <div className="create">
-      <h2>Add a New Blog</h2>
+      <h2>Edit Blog</h2>
       <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
@@ -37,8 +54,8 @@ const Create = () => {
         <label>Blog body:</label>
         <textarea
           required
-          value={body}
           rows="15"
+          value={body}
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
@@ -49,12 +66,10 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-      
-       
-        <button>Add Blog</button>
+        <button>Update Blog</button>
       </form>
     </div>
   );
 }
- 
-export default Create;
+
+export default Edit;
